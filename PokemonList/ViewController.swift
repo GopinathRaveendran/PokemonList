378e586d6d70354757d6a839c2ed503a88cdafaf
GopinathRaveendran <109667704+GopinathRaveendran@users.noreply.github.com>
2022-08-19 22:25:09 +0300

    Initial Commit

---
+++ PokemonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonList
+//
+//  Created by gopinath.raveendran on 19/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
